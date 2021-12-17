10fd360e4d0b7e8678e136f2969e694c37cbc79b
Sirinian Aram Emmanouil <ae.sirinian@gmail.com>
2021-12-17 14:04:58 +0200

    Created new project pfseven-smdb

---
+++ src/main/java/com/pfseven/smdb/PfsevenSmdbApplication.java
@@ -0,0 +1,13 @@
+package com.pfseven.smdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PfsevenSmdbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PfsevenSmdbApplication.class, args);
+	}
+
+}
