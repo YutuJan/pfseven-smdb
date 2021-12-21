dff61e18b6a30ef215169e46cc439117208d23d7
Sirinian Aram Emmanouil <ae.sirinian@gmail.com>
2021-12-21 11:30:39 +0200

    Created runner classes for development purposes.

---
+++ src/main/java/com/pfseven/smdb/bootstrap/GeneratePositionRunner.java
@@ -0,0 +1,18 @@
+package com.pfseven.smdb.bootstrap;
+
+import com.pfseven.smdb.base.AbstractLogComponent;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+
+@Component
+@Profile("dev")
+@RequiredArgsConstructor
+public class GeneratePositionRunner extends AbstractLogComponent implements CommandLineRunner {
+
+    @Override
+    public void run(String... args) throws Exception {
+
+    }
+}
