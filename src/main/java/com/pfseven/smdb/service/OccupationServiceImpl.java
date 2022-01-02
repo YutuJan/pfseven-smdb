fba4fddb848a4fcbf520c6dea9073db116d660c2
McDevil96 <dit16155@uop.gr>
2022-01-02 21:47:24 +0200

    updated episode and occupation service classes

--- src/main/java/com/pfseven/smdb/service/OccupationServiceImpl.java
+++ src/main/java/com/pfseven/smdb/service/OccupationServiceImpl.java
@@ -1,7 +1,6 @@
 package com.pfseven.smdb.service;
 
-import com.pfseven.smdb.domain.RoleType;
-import com.pfseven.smdb.domain.Occupation;
+import com.pfseven.smdb.domain.*;
 import com.pfseven.smdb.repository.OccupationRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -20,7 +19,42 @@ public class OccupationServiceImpl extends BaseServiceImpl<Occupation> implement
     }
 
     @Override
+    public void addOccupation(Person person, Movie movie, Occupation occupation) {
+
+    }
+
+    @Override
+    public void removeOccupation(Person person, Movie movie, Occupation occupation) {
+
+    }
+
+    @Override
+    public void updateOccupation(Person person, Movie movie, Occupation occupation) {
+
+    }
+
+    @Override
+    public void addOccupation(Person person, Episode episode, Occupation occupation) {
+
+    }
+
+    @Override
+    public void removeOccupation(Person person, Episode episode, Occupation occupation) {
+
+    }
+
+    @Override
+    public void updateOccupation(Person person, Episode episode, Occupation occupation) {
+
+    }
+
+    @Override
     public List<Occupation> findAll(RoleType role) {
+        return null;
+    }
+
+    @Override
+    public List<Occupation> getAll(RoleType role) {
         return null;
     }
 
