7d08fa49ca2f932e07791114b450f89fb7e147c8
Sirinian Aram Emmanouil <ae.sirinian@gmail.com>
2022-01-08 21:40:58 +0200

    Added missing import.

--- src/main/java/com/pfseven/smdb/controller/PersonController.java
+++ src/main/java/com/pfseven/smdb/controller/PersonController.java
@@ -5,6 +5,7 @@ import com.pfseven.smdb.service.BaseService;
 import com.pfseven.smdb.service.PersonService;
 import com.pfseven.smdb.transfer.ApiResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
