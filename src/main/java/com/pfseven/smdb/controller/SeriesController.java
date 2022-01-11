d26db499a3e2f20d17f6345fbd1246296a0358a0
Sirinian Aram Emmanouil <ae.sirinian@gmail.com>
2022-01-11 20:55:43 +0200

    Test

--- src/main/java/com/pfseven/smdb/controller/SeriesController.java
+++ src/main/java/com/pfseven/smdb/controller/SeriesController.java
@@ -13,7 +13,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
-
+//
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/series")
